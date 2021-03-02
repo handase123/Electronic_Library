32eed36c64d8d9f5bd2a2074783b81d54fed30c0
handase123 <maktabehandase@gmail.com>
2021-03-02 13:13:49 +0000

    paginate by 10

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ class PostListView(ListView):
     template_name = 'blog/home.htm'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 10
     queryset = Post.objects.all()
 
 class SearchView(ListView):
@@ -21,7 +21,7 @@ class SearchView(ListView):
     template_name = 'blog/search_bar.htm'
     context_object_name = 'posts'
     ordering = '-date_posted'
-    paginate_by = 2
+    paginate_by = 10
 
     def get_queryset(self):
         if self.request.method == 'GET':
@@ -36,7 +36,7 @@ class SearchView(ListView):
             else:
                 queryset = Post.objects.all()
             return list(set(queryset))
-    
+
 
 def AboutView(request):
     return render(request, 'blog/about.htm')
@@ -51,7 +51,7 @@ class CategoryCreateView(LoginRequiredMixin,CreateView):
         return super().form_valid(form)
 
 class CategoryUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
-    
+
     model = Category
     fields = ['category_name']
     template_name = 'blog/category_form.htm'
@@ -59,7 +59,7 @@ class CategoryUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
     def test_func(self):
         category = self.get_object()
         if self.request.user == category.author:
@@ -84,20 +84,20 @@ class CategoryListView(ListView):
     model = Category
     template_name = 'blog/categories.htm'
     context_object_name = 'categories'
-    paginate_by = 2
+    paginate_by = 10
     queryset = Category.objects.all()
 
 class PostCategoriesListView(ListView):
     model = Post
     template_name = 'blog/post_cat.htm'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 10
 
     def get_queryset(self):
         cat_pk = self.kwargs.get('pk')
         posts = Post.objects.all().filter(category_id=cat_pk)
         return posts
-    
+
 
 class PostDetailView(DetailView):
 
@@ -106,7 +106,7 @@ class PostDetailView(DetailView):
     context_object_name = 'post'
 
 class PostCreateView(LoginRequiredMixin,CreateView):
-    
+
     model = Post
     fields = ['title','content','book','category']
     template_name = 'blog/post_form.htm'
@@ -114,7 +114,7 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
 class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
 
     model = Post
@@ -124,7 +124,7 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
@@ -132,7 +132,7 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
         return False
 
 class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
-    
+
     model = Post
     success_url = '/'
     template_name = 'blog/post_confirm_delete.htm'
